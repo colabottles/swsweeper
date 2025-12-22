bcb1cb0ac6fee676079d0d32d3dda6dfd5d97dee
Todd Libby <toddlibby@protonmail.com>
2025-12-22 14:21:46 -0700

    Reset to new layout I hope

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://energy-template.vbartalis.dev",
-  output: "static",
-  trailingSlash: "ignore",
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  adapter: netlify(),
 });
