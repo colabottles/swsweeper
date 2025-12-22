f89f3de9907e2d03f27340d646bfe743c9766566
Todd Libby <toddlibby@protonmail.com>
2025-12-22 14:30:28 -0700

    Please be the one

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://energy-template.vbartalis.dev",
-  output: "static",
-  trailingSlash: "ignore",
-});
-=======
 // @ts-check
 
 import mdx from '@astrojs/mdx';
@@ -24,4 +12,3 @@ export default defineConfig({
   integrations: [mdx(), sitemap()],
   adapter: netlify(),
 });
->>>>>>> fcb02746200fb67f133bf9bf4407084ba5154c70
